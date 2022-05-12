b17c579b7f1831a5a14121768af0a088c887482b
istiak101 <30789544+istiak101@users.noreply.github.com>
2022-05-12 11:11:51 +0600

    Create rhel.reg.sh

---
+++ rhel.reg.sh
@@ -0,0 +1,8 @@
+subscription-manager remove
+subscription-manager unregister
+subscription-manager clean
+
+subscription-manager register
+
+subscription-manager refresh
+subscription-manager attach --pool <pool_id>
