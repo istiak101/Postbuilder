5f76808bff97f7b3b95cf921ce0cb8393153e1b6
istiak101 <30789544+istiak101@users.noreply.github.com>
2022-05-17 10:08:02 +0600

    Update rpmdev.sh

--- rpmdev.sh
+++ rpmdev.sh
@@ -4,12 +4,12 @@ dnf install -y mock
 architecture=$(arch)
 if echo $architecture | grep -q "aarch64"; then
   mock -r epel-7-aarch64 --init
-  mock -r epel-8-aarch64 --init
-  mock -r epel-9-aarch64 --init
+  mock -r rhel+epel-8-aarch64 --init
+  mock -r rhel+epel-9-aarch64 --init
 elif echo $architecture | grep -q "x86_64"; then
   mock -r epel-7-x86_64 --init
-  mock -r epel-8-x86_64 --init
-  mock -r epel-9-x86_64 --init
+  mock -r rhel+epel-8-x86_64 --init
+  mock -r rhel+epel-9-x86_64 --init
 fi
 
 useradd istiak
