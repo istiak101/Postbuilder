dd92ac83e9b8c8cab90a547dacdf18578a2d071f
Ice Lake <30789544+istiak101@users.noreply.github.com>
2022-07-23 02:50:27 +0600

    Update mailextract.sh

--- mailextract.sh
+++ mailextract.sh
@@ -8,6 +8,16 @@ gunzip -S "7" *
 gunzip -S "8" *
 gunzip -S "9" *
 
+gunzip -f -S "1" *
+gunzip -f -S "2" *
+gunzip -f -S "3" *
+gunzip -f -S "4" *
+gunzip -f -S "5" *
+gunzip -f -S "6" *
+gunzip -f -S "7" *
+gunzip -f -S "8" *
+gunzip -f -S "9" *
+
 gunzip -S "Sa" *
 gunzip -S "S" *
 gunzip -S "2," *
